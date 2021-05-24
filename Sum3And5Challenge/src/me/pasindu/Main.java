816d0f82d6177ca9ffbdb1040c3aa32a42d847af
pasindu <pasinduakalpa1998@gmail.com>
2021-05-24 21:25:22 +0530

    upload

---
+++ Sum3And5Challenge/src/me/pasindu/Main.java
@@ -0,0 +1,21 @@
+package me.pasindu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 0;
+        int sum = 0;
+
+        for (int i = 1; i < 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                sum += i;
+                count++;
+                System.out.println("Found number = " + i);
+            }
+            if (count == 5) {
+                break;
+            }
+        }
+        System.out.println("Sum of numbers = " + sum);
+    }
+}
---
+++ TheWhileAndDoWhileStatement/src/me/pasindu/Main.java
@@ -0,0 +1,12 @@
+package me.pasindu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count = 0;
+        while (count != 5) {
+            System.out.println("Count value is " + count);
+            count++;
+        }
+    }
+}
