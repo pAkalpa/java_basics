ff2e5ae84e59e920bd2442628069236bb93dbd4a
pasindu <pasinduakalpa1998@gmail.com>
2021-05-24 23:12:00 +0530

    upload

---
+++ DigitSumChallenge/src/me/pasindu/Main.java
@@ -0,0 +1,25 @@
+package me.pasindu;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("The sum of digits in number 125 is " + sumDigits(125));
+        System.out.println("The sum of digits in number -125 is " + sumDigits(-125));
+        System.out.println("The sum of digits in number 4 is " + sumDigits(4));
+        System.out.println("The sum of digits in number 32123 is " + sumDigits(32123));
+    }
+
+    private static int sumDigits(int number) {
+        if (number < 10) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
--- TheWhileAndDoWhileStatement/src/me/pasindu/Main.java
+++ TheWhileAndDoWhileStatement/src/me/pasindu/Main.java
@@ -3,10 +3,60 @@ package me.pasindu;
 public class Main {
 
     public static void main(String[] args) {
-        int count = 0;
-        while (count != 5) {
-            System.out.println("Count value is " + count);
-            count++;
+//        int count = 6;
+//        while (count != 6) {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+
+//        count = 6;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if (count > 100) {
+//                break;
+//            }
+//        } while (count != 6);
+
+//        for (int i = 1; i < 7; i++) {
+//            System.out.println("Count value is " + count);
+//        }
+//        for (count = 1; count != 6; count++) {
+//            System.out.println("Count value is " + count);
+//        }
+
+//        count = 1;
+//        while (true) {
+//            if (count == 6) {
+//                break;
+//            }
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+        int number = 4;
+        int finishNumber = 20;
+        int evenCount = 0;
+
+        while (number <= finishNumber) {
+            number++;
+            if (!isEvenNumber(number)) {
+                continue;
+            }
+
+            System.out.println("Even number " + number);
+            evenCount++;
+            if (evenCount == 5) {
+                break;
+            }
         }
+        System.out.println("Total even numbers found = " + evenCount);
+    }
+
+    private static boolean isEvenNumber(int number) {
+        if (number % 2 == 0) {
+            return true;
+        }
+        return false;
     }
 }
