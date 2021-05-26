3f9bc597fa3370b47368e8b0f84073d4cb3982fc
pasindu <pasinduakalpa1998@gmail.com>
2021-05-26 05:32:01 +0530

    upload

---
+++ ReadUserInput/src/me/pasindu/Main.java
@@ -0,0 +1,34 @@
+package me.pasindu;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your year of birth: ");
+
+        boolean hasNextInt = scanner.hasNextInt();
+
+
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2021 - yearOfBirth;
+
+            if ((age >= 0) && (age <= 100)) {
+                System.out.println("Your name is " + name + ", and you are " + age + " years old.");
+            } else {
+                System.out.println("Invalid Year of Birth");
+            }
+        } else {
+            System.out.println("unable to parse year of birth.");
+        }
+
+        scanner.close();
+    }
+}
